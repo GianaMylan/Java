a28a4818a76a035eb71bce02f5394501bca3bf3b
Giana Mylan <Giana Mylan>
2021-02-18 20:08:13 -0500

    updated doge look

--- Springboot/first-database-connection/src/main/java/com/example/firstdatabaseconnection/controller/DogeController.java
+++ Springboot/first-database-connection/src/main/java/com/example/firstdatabaseconnection/controller/DogeController.java
@@ -12,22 +12,27 @@ import org.springframework.web.bind.annotation.*;
 public class DogeController {
     @Autowired
     DogeService dogeService;
+
     @GetMapping
     public Iterable<Doges> getDoges() {
         return dogeService.getDoges();
     }
+
     @GetMapping("/{id}") // http://localhost:8080/doges/:id
     public Doges getDogeById(@PathVariable Long id) {
         return dogeService.findDogeById(id);
     }
+
     @PostMapping
     public Doges createDoge(@RequestBody Doges doge) {
         return dogeService.createDoge(doge);
     }
+
     @DeleteMapping("/{id}")
     public HttpStatus deleteDogeById(@PathVariable Long id) {
         return dogeService.deleteDoge(id);
     }
+
     @PatchMapping
     public Doges updateDoge(@RequestBody Doges doge) {
         return dogeService.updateDoge(doge);
